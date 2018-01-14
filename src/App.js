2c19ca40022d6ecc1f7787b8a6195396c3590f69
Erick <Erickva70@gmail.com>
2018-01-14 12:28:38 -0600

    probando componenetes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { Button } from "antd";
 import LayoutUI from "../src/components/Layaout"
 // import logo from './logo.svg';
-// import './App.css';
+//import './App.css';
 
 class App extends Component {
   render() {
--- src/components/Layaout.js
+++ src/components/Layaout.js
@@ -1,9 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+//import logo from './logo.svg';
+import '../App.css'
+import { Layout, Menu, Breadcrumb, Icon, Table } from 'antd';
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
+const dataSource = [{
+    key: '1',
+    name: 'Mike',
+    age: 32,
+    address: '10 Downing Street'
+  }, {
+    key: '2',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street'
+  }, {
+    key: '3',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street'
+  }, {
+    key: '4',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street'
+  }, {
+    key: '5',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street'
+  }
+];
+  
+  const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  }, {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  }, {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  }];
 
 class LayoutUI extends Component {
   render() {
@@ -17,9 +59,9 @@ class LayoutUI extends Component {
             defaultSelectedKeys={['2']}
             style={{ lineHeight: '64px' }}
           >
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
+            <Menu.Item key="1">Pedido Sugerido a Tiendas</Menu.Item>
+            {/* <Menu.Item key="2">nav 2</Menu.Item> */}
+            {/* <Menu.Item key="3">nav 3</Menu.Item> */}
           </Menu>
         </Header>
         <Layout>
@@ -57,7 +99,7 @@ class LayoutUI extends Component {
               <Breadcrumb.Item>App</Breadcrumb.Item>
             </Breadcrumb>
             <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
-              Content
+            <Table dataSource={dataSource} columns={columns} />
             </Content>
           </Layout>
         </Layout>
