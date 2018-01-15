1b90843299a3a8f1d532f8e6159736217541fb3a
Erick <Erickva70@gmail.com>
2018-01-14 22:04:47 -0600

    update fetch

--- src/components/TablaLayout.js
+++ src/components/TablaLayout.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Table } from 'antd';
-import reqwest from 'reqwest';
 
 const columns = [{
   title: 'Articulo',
@@ -14,46 +13,30 @@ const columns = [{
 }];
 
 class TabLay extends Component {
-  state = {
-    data: [],
-    pagination: {},
-    loading: false,
-  };
-  handleTableChange = (pagination, filters, sorter) => {
-    const pager = { ...this.state.pagination };
-    pager.current = pagination.current;
-    this.setState({
-      pagination: pager,
-    });
-    this.fetch({
-      results: pagination.pageSize,
-      page: pagination.current,
-      sortField: sorter.field,
-      sortOrder: sorter.order,
-      ...filters,
-    });
+  constructor(){
+    super();
+    this.state = {
+      data: [],
+      pagination: {},
+      loading: false,
+    }
   }
 
-  fetch = (params = {}) => {
+  componentDidMount(){
     this.setState({ loading: true });
-    reqwest({
-      url: 'http://localhost:3001/api/v1/',
-      method: 'GET',
-      type: 'json',
-    }).then((data) => {
+    fetch('http://localhost:3001/api/v1')
+    .then(results =>{
+      return results.json();
+    }).then(data => {
+      console.log(data.recordset)
       const pagination = { ...this.state.pagination };
-      // Read total count from server
-      // pagination.total = data.totalCount;
-      pagination.total = 500;
+      pagination.total = 200;
       this.setState({
         loading: false,
         data: data.recordset,
-        pagination,
+        pagination
       });
-    });
-  }
-  componentDidMount() {
-    this.fetch();
+    })
   }
   render() {
     return (
